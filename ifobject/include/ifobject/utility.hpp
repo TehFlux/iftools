a5392331580e03be9e362a1cec3a9684a0a7a021
Ionflux <webmaster@ionflux.org>
2008-02-17 01:26:59 +0000

    + Added utility functions (these come from IFTools and were included   to remove the IFTools dependency in other projects). + Added a classinfo feature to allow developers to enable the   generation of class information as necessary.

---
+++ ifobject/include/ifobject/utility.hpp
@@ -0,0 +1,190 @@
+#ifndef IONFLUX_OBJECT_UTILITY
+#define IONFLUX_OBJECT_UTILITY
+/* ==========================================================================
+ * Ionflux Object Base System
+ * Copyright © 2006 Joern P. Meier
+ * mail@ionflux.org
+ * --------------------------------------------------------------------------
+ * utility.hpp                 Utility functions (header).
+ * ==========================================================================
+ *
+ * This file is part of Ionflux Object Base System.
+ * 
+ * Ionflux Object Base System is free software; you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * Ionflux Object Base System is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with Ionflux Object Base System; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * ========================================================================== */
+
+#include <string>
+#include <vector>
+
+namespace Ionflux
+{
+
+namespace ObjectBase
+{
+
+class IFObject;
+
+/// Default size for read buffers.
+const unsigned int READ_BUFFER_SIZE = 4096;
+
+/** Read file.
+ *
+ * Read data from a file.
+ *
+ * \param fileName File name.
+ * \param fileName Where to store the data.
+ * \param logTarget Target for logging messages.
+ *
+ * \return \c true if the file can be read from, \c false otherwise.
+ */
+bool readFile(const std::string& fileName, std::string& target, 
+    Ionflux::ObjectBase::IFObject* logTarget = 0);
+
+/** Explode a string.
+ *
+ * Splits up a string of bytes at 'splitString', creating a vector containing 
+ * the separated elements.
+ *
+ * \param bytes String to be exploded.
+ * \param splitString Separator string.
+ * \param result Vector to store the result in.
+ */
+void explode(const std::string& bytes, const std::string& splitString, 
+	std::vector<std::string>& result);
+
+/** Check type of a character.
+ *
+ * Returns \c true if the character 'c' is one of the characters of
+ * 'testChars'.
+ *
+ * \param c Character to be checked.
+ * \param testChars String of characters.
+ *
+ * \return \c true if the character 'c' is one of the characters of
+ * 'testChars', \c false otherwise.
+ */
+bool isOneOf(char c, const std::string& testChars);
+
+/** Check whether a string represents a number.
+ *
+ * Returns \c true if the string represents a (decimal) number, \c false 
+ * otherwise.
+ *
+ * \param bytes String to be checked.
+ *
+ * \return \c true if the string represents a (decimal) number, \c false 
+ * otherwise.
+ */
+bool isNumber(const std::string &bytes);
+
+/** Check whether a string represents an alpha.
+ *
+ * Returns \c true if the string represents a (latin) alpha, \c false 
+ * otherwise.
+ *
+ * \param bytes String to be checked.
+ *
+ * \return \c true if the string represents a (latin) alpha, \c false 
+ * otherwise.
+ */
+bool isAlpha(const std::string &bytes);
+
+/** Check whether a string represents an integer.
+ *
+ * Returns \c true if the string represents a (decimal) integer value 
+ * (positive or negative), \c false otherwise.
+ *
+ * \param bytes String to be checked.
+ *
+ * \return \c true if the string represents a (decimal) integer, \c false 
+ * otherwise.
+ */
+bool isInteger(const std::string &bytes);
+
+/** Check whether a string represents a float.
+ *
+ * Returns \c true if the string represents a (decimal) floating point value 
+ * (positive or negative), \c false otherwise.
+ *
+ * \param bytes String to be checked.
+ *
+ * \return \c true if the string represents a (decimal) floating point value, 
+ * \c false otherwise.
+ */
+bool isFloat(const std::string &bytes);
+
+/** Check whether a string represents an identifier.
+ *
+ * Returns \c true if the string represents an idenifier, \c false otherwise.
+ *
+ * \note For purposes of this function, an identifier is a byte string 
+ * consisting of alphanumeric characters or underscores '_' (in 8-bit ASCII 
+ * encoding).
+ *
+ * \param bytes String to be checked.
+ *
+ * \return \c true if the string represents an idenifier, \c false otherwise.
+ */
+bool isIdentifier(const std::string &bytes);
+
+/** Convert string to upper case.
+ *
+ * Convert a text string to upper case if possible.
+ *
+ * \param text Input text string.
+ * \param numChars Maximum number of characters to convert.
+ * \param offset Index where to start case conversion.
+ *
+ * \return Upper case string.
+ */
+std::string toUpper(const std::string &text, unsigned int numChars = 0, 
+	unsigned int offset = 0);
+
+/** Convert string to lower case.
+ *
+ * Convert a text string to lower case if possible.
+ *
+ * \param text Input text string.
+ * \param numChars Maximum number of characters to convert.
+ * \param offset Index where to start case conversion.
+ *
+ * \return Lower case string.
+ */
+std::string toLower(const std::string &text, unsigned int numChars = 0, 
+	unsigned int offset = 0);
+
+/** Trim byte string.
+ *
+ * Remove whitespace characters from the beginning and end of the specified 
+ * string.
+ *
+ * \param bytes Byte string.
+ * \param leftTrim Remove whitespace from beginning of string.
+ * \param rightTrim Remove whitespace from end of string.
+ *
+ * \return String with whitespace removed from beginning and end.
+ */
+std::string trim(const std::string& bytes, bool leftTrim = true, 
+	bool rightTrim = true);
+
+}
+
+}
+
+/** \file utility.hpp
+ * \brief Utility functions (header).
+ */
+#endif
---
+++ ifobject/src/utility.cpp
@@ -0,0 +1,258 @@
+/* ==========================================================================
+ * Ionflux Object Base System
+ * Copyright © 2006 Joern P. Meier
+ * mail@ionflux.org
+ * --------------------------------------------------------------------------
+ * utility.cpp                 Utility functions (implementation).
+ * ==========================================================================
+ *
+ * This file is part of Ionflux Object Base System.
+ * 
+ * Ionflux Object Base System is free software; you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * Ionflux Object Base System is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with Ionflux Object Base System; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * ========================================================================== */
+
+#include <sstream>
+#include <fstream>
+#include "ifobject/utility.hpp"
+#include "ifobject/IFObject.hpp"
+#include "ifobject/IFLogMessage.hpp"
+
+using namespace std;
+
+namespace Ionflux
+{
+
+namespace ObjectBase
+{
+
+bool readFile(const std::string& fileName, std::string& target, 
+    Ionflux::ObjectBase::IFObject* logTarget)
+{
+	ifstream inFile(fileName.c_str());
+    if (!inFile)
+    {
+        if (logTarget != 0)
+        {
+            ostringstream message;
+            message << "Could not open file '" << fileName << "'.";
+            logTarget->log(IFLogMessage(message.str(), VL_ERROR, 0, 
+                "readFile"));
+        }
+        return false;
+    }
+	char *buffer = new char[READ_BUFFER_SIZE];
+	int bytesRead;
+	inFile.read(buffer, READ_BUFFER_SIZE);
+	bytesRead = inFile.gcount();
+    target = "";
+	while (bytesRead > 0)
+	{
+		target.append(buffer, bytesRead);
+		inFile.read(buffer, READ_BUFFER_SIZE);
+		bytesRead = inFile.gcount();
+	}
+	delete[] buffer;
+    return true;
+}
+
+void explode(const std::string& bytes, const std::string& splitString, 
+	std::vector<std::string>& result)
+{
+	result.clear();
+	if ((bytes.size() == 0) 
+        || (splitString.size() == 0))
+		return;
+	string::size_type currentPos = 0;
+	string::size_type nextPos;
+	while ((currentPos < bytes.size()) 
+		&& ((nextPos = bytes.find(splitString, currentPos)) != string::npos))
+	{
+		if ((nextPos - currentPos) > 0)
+			result.push_back(bytes.substr(currentPos, nextPos - currentPos));
+		else
+			result.push_back("");
+		currentPos = nextPos + 1;
+	}
+	result.push_back(bytes.substr(currentPos, bytes.size() - currentPos));
+}
+
+bool isOneOf(char c, const std::string& testChars)
+{
+	if (testChars.size() == 0)
+		return false;
+	if (testChars.find_first_of(c) == string::npos)
+		return false;
+	return true;
+}
+
+bool isNumber(const std::string &bytes)
+{
+	if (bytes.size() == 0)
+		return false;
+	bool result = true;
+	unsigned int i = 0;
+	while (result && (i < bytes.size()))
+	{
+		result = (result && isOneOf(bytes[i], "0123456789"));
+		i++;
+	}
+	return result;
+}
+
+bool isInteger(const std::string &bytes)
+{
+	if (bytes.size() == 0)
+		return false;
+	bool result = true;
+	unsigned int i = 0;
+	// Check signum.
+	if ((bytes.size() > 1) && isOneOf(bytes[i], "+-"))
+		i++;
+	// Check integer part.
+	while (result && (i < bytes.size()))
+	{
+		result = (result && isOneOf(bytes[i], "0123456789"));
+		i++;
+	}
+	return result;
+}
+
+bool isFloat(const std::string &bytes)
+{
+	if (bytes.size() == 0)
+		return false;
+	const string numbers = "0123456789";
+	const string dots = ".";
+	const string exponent = "eE";
+	const string signum = "+-";
+	unsigned int i = 0;
+	if (bytes.size() == 1)
+		return isOneOf(bytes[0], numbers);
+	// Check signum.
+	if (isOneOf(bytes[i], signum))
+		i++;
+	bool result = true;
+	// Check integer part.
+	while (result && (i < bytes.size()))
+	{
+		result = (result && isOneOf(bytes[i], numbers));
+		i++;
+	}
+	/* Accept dot if it is either preceded by a number (thus i > 1) 
+	   or the next character is a number. */
+	if (isOneOf(bytes[i - 1], dots) 
+		&& ((i > 1) || ((i < bytes.size()) && isOneOf(bytes[i], numbers))))
+		result = true;
+	// Check decimal part.
+	while (result && (i < bytes.size()))
+	{
+		result = (result && isOneOf(bytes[i], numbers));
+		i++;
+	}
+	if (isOneOf(bytes[i - 1], exponent))
+		result = true;
+	// Check signum of exponent.
+	if ((i < bytes.size()) && isOneOf(bytes[i], signum))
+		i++;
+	// Check exponent.
+	while (result && (i < bytes.size()))
+	{
+		result = (result && isOneOf(bytes[i], numbers));
+		i++;
+	}
+	return result;
+}
+
+bool isIdentifier(const std::string &bytes)
+{
+	const string identifierChars = "abcdefghijklmnopqrstuvwxyz"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+	bool result = true;
+	for (unsigned int i = 0; i < bytes.size(); i++)
+		result = (result && isOneOf(bytes[i], identifierChars));
+	return result;
+}
+
+std::string toUpper(const std::string &text, unsigned int numChars, 
+	unsigned int offset)
+{
+	ostringstream result;
+	char currentChar;
+	if (numChars == 0)
+		numChars = text.size();
+	unsigned int count = 0;
+	for (unsigned int i = 0; i < text.size(); i++)
+	{
+		if ((i >= offset) && (count < numChars))
+		{
+			currentChar = static_cast<char>(toupper(text[i]));
+			count++;
+		} else
+			currentChar = text[i];
+		result << currentChar;
+	}
+	return result.str();
+}
+
+std::string toLower(const std::string &text, unsigned int numChars, 
+	unsigned int offset)
+{
+	ostringstream result;
+	char currentChar;
+	if (numChars == 0)
+		numChars = text.size();
+	unsigned int count = 0;
+	for (unsigned int i = 0; i < text.size(); i++)
+	{
+		if ((i >= offset) && (count < numChars))
+		{
+			currentChar = static_cast<char>(tolower(text[i]));
+			count++;
+		} else
+			currentChar = text[i];
+		result << currentChar;
+	}
+	return result.str();
+}
+
+std::string trim(const std::string& bytes, bool leftTrim, bool rightTrim)
+{
+	if (bytes.size() == 0)
+		return "";
+	unsigned int from = 0;
+	unsigned int to = bytes.size() - 1;
+	static const string trimChars = " \t\r\n";
+	if (leftTrim)
+		while ((from <= to)
+			&& isOneOf(bytes[from], trimChars))
+			from++;
+	if (rightTrim)
+		while ((to >= from)
+			&& isOneOf(bytes[to], trimChars))
+			to--;
+	string result;
+	for (unsigned int i = from; i <= to; i++)
+		result.append(1, bytes[i]);
+	return result;
+}
+
+}
+
+}
+
+/** \file utility.cpp
+ * \brief Utility functions (implementation).
+ */
